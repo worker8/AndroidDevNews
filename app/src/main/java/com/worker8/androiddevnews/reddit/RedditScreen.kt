28d87d3697ba031a1f1bd98f49887651caa7298c
Tan Jun Rong <tanjunrong@gmail.com>
2021-12-16 00:44:17 +0900

    add progress abr for reddit screen to indicate loading state

--- app/src/main/java/com/worker8/androiddevnews/reddit/RedditScreen.kt
+++ app/src/main/java/com/worker8/androiddevnews/reddit/RedditScreen.kt
@@ -4,16 +4,13 @@ import android.content.Intent
 import android.net.Uri
 import android.webkit.MimeTypeMap
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -53,36 +50,49 @@ fun RedditList(
 ) {
     val context = LocalContext.current
 //    CompositionLocalProvider(LocalImageLoader provides ImageLoader(LocalContext.current)) {
-    LazyColumn(
-        modifier = Modifier
-            .semantics { testTag = "AAABBBB" }
-            .background(MaterialTheme.colors.background)
-            .fillMaxHeight(),
-        state = lazyListState
-    ) {
-        items(
-            count = state.value.count(),
-            itemContent = { index ->
-                val submission = state.value[index]
+    if (state.value.isEmpty()) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            CircularProgressIndicator()
+        }
+    } else {
+        LazyColumn(
+            modifier = Modifier
+                .semantics { testTag = "AAABBBB" }
+                .background(MaterialTheme.colors.background)
+                .fillMaxHeight(),
+            state = lazyListState
+        ) {
+            items(
+                count = state.value.count(),
+                itemContent = { index ->
+                    val submission = state.value[index]
 
-                RedditContentCard(
-                    submission = submission,
-                    truncation = true,
-                    onLinkClick = { submission ->
-                        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(submission.url))
-                        context.startActivity(browserIntent)
-                    }) {
-                    val intent = Intent(context, RedditDetailActivity::class.java).apply {
-                        putExtra(SubmissionKey, submission)
+                    RedditContentCard(
+                        submission = submission,
+                        truncation = true,
+                        onLinkClick = { submission ->
+                            val browserIntent =
+                                Intent(Intent.ACTION_VIEW, Uri.parse(submission.url))
+                            context.startActivity(browserIntent)
+                        }) {
+                        val intent = Intent(context, RedditDetailActivity::class.java).apply {
+                            putExtra(SubmissionKey, submission)
+                        }
+                        context.startActivity(intent)
                     }
-                    context.startActivity(intent)
+                    Divider(
+                        color = Color.LightGray,
+                        thickness = 1.dp
+                    )
                 }
-                Divider(
-                    color = Color.LightGray,
-                    thickness = 1.dp
-                )
-            }
-        )
+            )
+        }
     }
 }
 
